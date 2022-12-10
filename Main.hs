9f0ba4d37badfdc11790feb494d3a21bb1949585
Marta <117594252+matsuurans@users.noreply.github.com>
2022-12-10 20:14:41 +0000

    Add files via upload

---
+++ Main.hs
@@ -0,0 +1,21 @@
+{-
+Princípios de Programação 2022/2023
+Trabalho 4 - Modelo de submissão
+
+* A vossa submissão deverá ser composta por um único ficheiro zip
+t4_XXXXX_YYYYY.zip onde XXXXX, YYYYY são os vossos números de aluno
+por ordem crescente.
+* O ficheiro zip deverá conter no mínimo um ficheiro com o nome Main.hs
+* O vosso código deverá ser compilável com uma instrução do tipo
+
+$ stack ghc Main.hs
+
+A instrução acima produz um executável Main, que deverá ser executável
+através de um dos seguintes três tipos de instruções:
+
+$ ./Main [ficheiro] -- carrega um ficheiro para jogar
+$ ./Main            -- carrega o ficheiro default.map
+$ ./Main -t         -- corre os testes
+-}
+
+main = undefined
