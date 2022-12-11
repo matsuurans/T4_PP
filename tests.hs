bbcc2152de095e43d0b36d3adbd21229ed9dfeab
Inês Luz <inesluz.17@gmail.com>
2022-12-11 21:50:48 +0000

    Update tests.hs

--- tests.hs
+++ tests.hs
@@ -8,8 +8,8 @@ prop_move_labLength ej moveArg = length (labirinto ej) == length (labirinto(move
 prop_move_offLimits :: EstadoJogo -> String -> Bool
 prop_move_offLimits ej moveArg = posicaoValida (labirinto ej) (jogador (moveEJ ej moveArg))
 
-prop_move_sameKeys :: EstadoJogo -> String -> Bool
-prop_move_sameKeys ej moveArg = chaves (moveEJ ej moveArg) >= chaves ej 
+prop_move_correctKeys :: EstadoJogo -> String -> Bool
+prop_move_correctKeys ej moveArg = chaves (moveEJ ej moveArg) >= chaves ej 
 
-prop_move_sameDoors :: EstadoJogo -> String -> Bool
-prop_move_sameDoors ej moveArg = undefined
+prop_move_correctDoors :: EstadoJogo -> String -> Bool
+prop_move_correctDoors ej moveArg = contaPortas (labirinto (moveEJ ej moveArg)) <= contaPortas (labirinto ej)
