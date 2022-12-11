ba221af63c819f33464faefe9f1d47ccc09fd85e
Marta <117594252+matsuurans@users.noreply.github.com>
2022-12-11 22:37:05 +0000

    Update tests.hs

--- tests.hs
+++ tests.hs
@@ -9,7 +9,7 @@ prop_move_offLimits :: EstadoJogo -> String -> Bool
 prop_move_offLimits ej moveArg = posicaoValida (labirinto ej) (jogador (moveEJ ej moveArg))
 
 prop_move_correctKeys :: EstadoJogo -> String -> Bool
-prop_move_correctKeys ej moveArg = chaves (moveEJ ej moveArg) >= chaves ej 
+prop_move_correctKeys ej moveArg = length (chaves $ moveEJ ej moveArg) >= length (chaves ej)
 
 prop_move_correctDoors :: EstadoJogo -> String -> Bool
 prop_move_correctDoors ej moveArg = contaPortas (labirinto (moveEJ ej moveArg)) <= contaPortas (labirinto ej)
