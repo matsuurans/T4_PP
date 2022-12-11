6ba4b7db6709ac3c1b60bf6681603512e4b16330
Marta <117594252+matsuurans@users.noreply.github.com>
2022-12-11 02:53:58 +0000

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -19,16 +19,63 @@ $ ./Main -t         -- corre os testes
 -}
 
 import System.Environment
+import Control.Monad
 import Labirintos
 
 main = do
     args <- getArgs
     content <- readFile (getFile args)
-    putStrLn (getLab content)
+    let ej = EstadoJogo (lines $ getLab content) (getPlayer content) (getKeys content) False
+    execute ej
 
+execute :: EstadoJogo -> IO ()
+execute ej = do
+    putStrLn $ toString ej
+    read <- getLine
+    let command = head $ words read
+    let input = head $ tail $ words read
+    if command == "move"
+        then move input ej
+        else if command == "load"
+            then load input
+            else if command == "save"
+                then save input ej
+                else when (command == "exit") exit
+
+move :: String -> EstadoJogo -> IO ()
+move dir ej = do
+    let newEJ = moveEJ ej dir
+    execute newEJ
+
+load :: String -> IO ()
+load ficheiro = do
+    content <- readFile (getFile [ficheiro])
+    let ej = EstadoJogo (lines $ getLab content) (getPlayer content) (getKeys content) False
+    execute ej
+
+save :: String -> EstadoJogo -> IO ()
+save ficheiro ej = do
+    writeFile ficheiro (toFile ej)
+    execute ej
+
+exit :: IO ()
+exit = do
+    return ()
+
+getFile :: [String] -> String
 getFile args = if not (null args) then head args else "default.map"
 
+getPlayer :: String -> (Int,Int)
+getPlayer content = read $ head $ lines content :: (Int,Int)
+
+getKeys :: String -> String
+getKeys content = head $ tail $ lines content
+
+getLab :: String -> String
 getLab [] = ""
 getLab (c:s)
     | c == '*' = c:s
     | otherwise = getLab s
+
+toFile :: EstadoJogo -> String
+toFile ej = show (jogador ej) ++ "\n" ++ chaves ej ++ unlines (labirinto ej)
