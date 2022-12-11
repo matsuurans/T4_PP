83eda2252d7e0c2bee6c4b10fb8c0c49771bb38a
Marta <117594252+matsuurans@users.noreply.github.com>
2022-12-11 03:09:45 +0000

    Update tests.hs

--- tests.hs
+++ tests.hs
@@ -3,13 +3,13 @@ import Labirintos
 
 -- Testa se o labirinto resultante do move tem a mesma dimensão antes da sequência de movimentos
 prop_move_labLength :: EstadoJogo -> String -> Bool
-prop_move_labLength ej moveArg = length (labirinto ej) == length (labirinto(move ej moveArg))
+prop_move_labLength ej moveArg = length (labirinto ej) == length (labirinto(moveEJ ej moveArg))
 
 prop_move_offLimits :: EstadoJogo -> String -> Bool
-prop_move_offLimits ej moveArg = posicaoValida (labirinto ej) (jogador (move ej moveArg))
+prop_move_offLimits ej moveArg = posicaoValida (labirinto ej) (jogador (moveEJ ej moveArg))
 
 prop_move_sameKeys :: EstadoJogo -> String -> Bool
-prop_move_sameKeys ej moveArg = chaves (move ej moveArg) >= chaves ej 
+prop_move_sameKeys ej moveArg = chaves (moveEJ ej moveArg) >= chaves ej 
 
 prop_move_sameDoors :: EstadoJogo -> String -> Bool
 prop_move_sameDoors ej moveArg = undefined
