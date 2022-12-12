9a7ae41884653430decd4fd21c8bdf118823c9c8
Marta <117594252+matsuurans@users.noreply.github.com>
2022-12-12 17:16:37 +0000

    Update tests.hs

--- tests.hs
+++ tests.hs
@@ -1,5 +1,23 @@
 import Labirintos
+import Test.QuickCheck
+import System.Random
+import Control.Monad
 
+newtype Movimentos = Movimentos String
+dir :: Gen Char
+dir = elements "ulrd"
+instance Arbitrary Movimentos where
+    arbitrary = Movimentos <$> listOf dir
+
+{--- para testar com o sample (arbitrary :: Gen Movimentos)
+instance Show Movimentos where
+    show (Movimentos mov) = show mov-}
+
+instance Arbitrary EstadoJogo where
+    arbitrary = do
+        lab <- elements labirintos
+        let pos = procura lab 'S' 0
+        return $ EstadoJogo lab pos "" False
 
 -- Testa se o labirinto resultante do move tem a mesma dimensão antes da sequência de movimentos
 prop_move_labLength :: EstadoJogo -> String -> Bool
@@ -18,18 +36,20 @@ prop_move_correctDoors ej moveArg = contaPortas (labirinto (moveEJ ej moveArg)) 
 
 prop_move_correctPortals :: EstadoJogo -> String -> Bool
 prop_move_correctPortals ej moveArg
-    | jogador (moveEJ ej moveArg) /= posicaoPortal1 (labirinto ej)
-        && jogador (moveEJ ej moveArg) /= posicaoPortal2 (labirinto ej) (posicaoPortal1 (labirinto ej)) = existem2PortaisChar (labirinto (moveEJ ej moveArg))
-    | jogador (moveEJ ej moveArg) == posicaoPortal1 (labirinto ej)
-        || jogador (moveEJ ej moveArg) == posicaoPortal2 (labirinto ej) (posicaoPortal1 (labirinto ej)) = existe1PortalChar (labirinto (moveEJ ej moveArg))
+    | player /= portal1 && player /= portal2 = portais == 2
+    | player == portal1 || player == portal2 = portais == 1
     | otherwise = False
+    where newEJ = moveEJ ej moveArg
+          lab = labirinto ej
+          player = jogador newEJ
+          portal1 = posicaoPortal1 lab
+          portal2 = posicaoPortal2 lab portal1
+          portais = contaCaracteres '@' lab
 
 
 -- se nao existir uma posicao de 'F' depois do move --> terminado tem de ser True
 -- se existir uma posicao de 'F' depois do move --> terminado tem de ser False
 prop_move_finishCorrectly :: EstadoJogo -> String -> Bool
-prop_move_finishCorrectly ej moveArg = ((procura (labirinto (moveEJ ej moveArg)) 'F' 0 == (-1,-1))
-                                        && terminado (moveEJ ej moveArg))
-                                        ||
-                                       ((procura (labirinto (moveEJ ej moveArg)) 'F' 0 /= (-1,-1))
-                                        && not (terminado (moveEJ ej moveArg)))
+prop_move_finishCorrectly ej moveArg = (posF == (-1,-1) && over) || (posF /= (-1,-1) && not over)
+    where posF = procura (labirinto $ moveEJ ej moveArg) 'F' 0
+          over = terminado $ moveEJ ej moveArg
